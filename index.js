5ee336025a187b897a6eb2a297fa939e95d5559e
RidzherIT <116496741+RidzherIT@users.noreply.github.com>
2023-01-16 00:05:31 +0400

    Add Vercel-CORS

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const fileUpload = require('express-fileupload');
-const userRouter = require('./router/user.router');
-const customerRouter = require('./router/customer.router');
-const productRouter = require('./router/product.router');
-const ordersRouter = require('./router/orders.router');
+const express = require("express");
+const cors = require("vercel-cors");
+const path = require("path");
+const fileUpload = require("express-fileupload");
+const userRouter = require("./router/user.router");
+const customerRouter = require("./router/customer.router");
+const productRouter = require("./router/product.router");
+const ordersRouter = require("./router/orders.router");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors({
+app.use(
+  cors({
     origin: "*",
-    methods: "GET, POST, PUT, DELETE"
-}));
+    methods: ["GET", "POST", "PUT", "PATCH", "UPDATE", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use(fileUpload({}));
-app.use(express.static(path.resolve(__dirname, 'static')));
-app.use('/api', userRouter);
-app.use('/customer', customerRouter);
-app.use('/product', productRouter);
-app.use('/orders', ordersRouter);
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use("/api", userRouter);
+app.use("/customer", customerRouter);
+app.use("/product", productRouter);
+app.use("/orders", ordersRouter);
 app.listen(PORT, () => `Server started on PORT ${PORT}`);
-
